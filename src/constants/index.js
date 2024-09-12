ea5458daa1261dfc36fcd1fd0330ff7cfe9db932
Sarthak1306 <sarthak13062001@gmail.com>
2024-09-12 15:47:43 +0530

    index

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "DevOps Enthusiast",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Generative AI Practitioner",
       icon: creator,
     },
   ];
